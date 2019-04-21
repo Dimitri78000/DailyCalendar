9cd5311bf9765e05f6e35a331be030d7b85cebeb
Dimitri LEURS <dimitri.leurs@telecom-sudparis.eu>
2019-04-21 23:50:45 +0200

    On avance sur le projet - OpenClassroom/Django/Les Templates

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,23 @@
-from django.http import HttpResponse
+from datetime import datetime
 from django.shortcuts import render
 
-def home(request):
-    """ Exemple de page non valide au niveau HTML pour que l'exemple soit concis """
-    return HttpResponse("""
-        <h1>Bienvenue sur mon blog !</h1>
-        <p>Les crêpes bretonnes ça tue des mouettes en plein vol !</p>
-    """)
+def date_actuelle(request):
+	couleurs = {
+	    'FF0000':'rouge', 
+	    'ED7F10':'orange', 
+	    'FFFF00':'jaune', 
+	    '00FF00':'vert', 
+	    '0000FF':'bleu', 
+	    '4B0082':'indigo', 
+	    '660099':'violet',
+	}
 
+	date = datetime.now().strftime("%A %d %B %Y - %H:%M")
+	return render(request, 'blog/date.html', locals())
+
+
+def addition(request, nombre1, nombre2):    
+    total = nombre1 + nombre2
+
+    # Retourne nombre1, nombre2 et la somme des deux au tpl
+    return render(request, 'blog/addition.html', locals())
